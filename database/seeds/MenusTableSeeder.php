c84decafaed76abbb229465df4fdf3ca87acfb81
Youngdee <echonull@qq.com>
2018-01-30 16:01:38 +0800

    忘记menus数据表的默认数据填充

--- database/seeds/MenusTableSeeder.php
+++ database/seeds/MenusTableSeeder.php
@@ -12,5 +12,13 @@ class MenusTableSeeder extends Seeder
     public function run()
     {
         //
+        DB::table('menus')->insert([
+            'name' => "测试栏目",
+            'icon' => 'link',
+            'url' => '#',
+            'parent_id' => '0',
+            'menu_order' => '0',
+            'name_index' => 'test'
+        ]);
     }
 }
